7e46acebba5d17b81ec9cb54a5a82d09fbd3ca65
chicodevbr <chicodevbr@gmail.com>
2022-05-22 15:33:18 -0300

    feat: :goal_net: adding error handler

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
+
 import { endpoint, postRequest, url } from '../api';
+import { capitalize } from '../helpers/functions';
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  user: '',
+  error: null,
   onLogout: () => {},
   onLogin: (name, password) => {},
   onSignup: (name, password) => {},
@@ -10,6 +14,8 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState('');
 
   useEffect(() => {
     const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
@@ -27,10 +33,12 @@ export const AuthContextProvider = ({ children }) => {
       );
       if (response.data.ok) {
         localStorage.setItem('isLoggedIn', 'ok');
+
+        setUser(capitalize(response.data.user.name));
         setIsLoggedIn(true);
       }
     } catch (error) {
-      console.log(error);
+      setError(error.message);
       return error.message;
     }
   };
@@ -43,10 +51,13 @@ export const AuthContextProvider = ({ children }) => {
       );
       if (response.data.ok) {
         localStorage.setItem('isLoggedIn', 'ok');
+
         setIsLoggedIn(true);
+        setUser(capitalize(response.data.login));
       }
     } catch (error) {
-      console.log(error);
+      console.log(error.message);
+      setError(error.message);
       return error.message;
     }
   };
@@ -54,12 +65,15 @@ export const AuthContextProvider = ({ children }) => {
   const logoutHandler = () => {
     localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
+    setUser('');
   };
 
   return (
     <AuthContext.Provider
       value={{
         isLoggedIn: isLoggedIn,
+        user: user,
+        error: error,
         onSignup: signupHandler,
         onLogin: loginHandler,
         onLogout: logoutHandler,
